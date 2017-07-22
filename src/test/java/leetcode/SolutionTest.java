fd7d210f77fde6abc247d9523da903e39930bdf3
skinwen <skinwen@gmail.com>
2017-07-22 13:41:44 +0800

    test

---
+++ src/test/java/leetcode/SolutionTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2017/7/22 0022.
+ */
+public class SolutionTest {
+    private Solution solution = new Solution();
+
+    @Test
+    public void twoSum() throws Exception {
+
+    }
+
+}
